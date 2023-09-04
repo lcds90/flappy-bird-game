0d11988c90cff25c6824139616ebd12e5f8e54a8
Leonardo Santos <lcds90@gmail.com>
2023-09-03 23:01:37 -0300

    chore: estrutura inicial do projeto 💫

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="game-main">
+    <game-canvas />
+  </main>
+</template>
+
+<style>
+.game-main {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+</style>
